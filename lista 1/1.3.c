79f22b657eb9eae40e30750cd80b2eed7580cee9
Luiza Munis <luizamunisnascimento@gmail.com>
2023-03-21 21:02:31 -0300

    att

--- lista 1/1.3.c
+++ lista 1/1.3.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 int main(){
 
-int n1;
-float n2;
-double n3;
+int numeroint;
+float numeroreal1;
+double numeroreal2;
 char letra,  palavra [20];
-
+ 
 printf(" Digite um numeiro inteiro: ");
-scanf("%d",&n1);
+scanf("%d",&numeroint);
 
 printf(" Digite um numero real: ");
-scanf("%f",&n2);
+scanf("%f",&numeroreal1);
 fflush (stdin);
 
 printf(" Digite um numero real com duas casas: ");
-scanf("%lf",&n3);
+scanf("%lf",&numeroreal2);
 fflush(stdin);
 
 printf(" Digite uma letra: ");
@@ -25,10 +25,9 @@ printf(" Digite uma palavra:");
 fgets(palavra,20,stdin);
 fflush(stdin);
 
-
-printf("\n Numeros reais: %d\n", n1);
-printf(" Os numeros reais sao:\n %.3f\n %.2lf\n", n2,n3);
+printf("\n Numeros reais: %d\n", numeroint);
+printf(" Os numeros reais sao:\n %.1f\n %.3lf\n", numeroreal1,numeroreal2);
 printf(" Letra: %c\n", letra);
 printf(" Palavra: %s\n", palavra);
-printf("Endereco: %i");
+printf("Endereco: %x", &numeroint ); 
 }
