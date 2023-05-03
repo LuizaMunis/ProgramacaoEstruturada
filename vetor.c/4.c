2832f496968eeb2961287a819c777dca31ecd995
Luiza Munis <luizamunisnascimento@gmail.com>
2023-05-02 21:27:26 -0300

    att

--- vetor.c/4.c
+++ vetor.c/4.c
@@ -1,27 +1,31 @@
 #include<stdio.h>
+#include<string.h>
 int main(){
 
-    int n[6];
-    int i, verificar, posicao[6], contador=0;
+    int n[80];
+    int i=0, verificar, posicao[80], contador=0,i2;
+    char opcao[3];
 
-    for(i=0;i<6;i++){
-        printf("DIGITE UM NUMERO: ");
-        scanf("%d",&n[i]);
-        //n[i]=i*5;
-    }
 
-    printf("DIGITE O NUMERO QUE QUER:");
-    scanf("%d",&verificar);
-
-    for(i=0;i<6;i++){
+       do{
+          printf("DIGITE UM NUMERO: ");
+          scanf("%d",&n[i]);
+          i++;
+          printf("Voce quer digitar mais um numero? ");
+          gets (opcao);
+        }while(strcmp (opcao,"sim")==0 && i<80);
+ 
+      i2=i
+    for(i=0;i<i2;i++){
         if(verificar==n[i]){
         posicao[contador]=i;
         contador++;
         }
     }
+
     printf("O numero %d aparece: %d vezes \n", verificar, contador);
     for(i=0;i<contador;i++){
         printf("Posicao: %d\n", posicao[i]);
     }
 
-}
+}  
